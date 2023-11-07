ee8309a8c56c73819929319835e04b76452bc6c2
anlere <anlere98@gmail.com>
2023-11-06 20:14:39 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//alert("Hola este es mi Javascript");
+
+//let nombre = "Marta";
+//nombre= "Maria";
+
+//var nombre = "Marta";
+
+//const nombre = "Marta";
+// //nombre2= "Maria""
+
+//console.log ("nombre");
+//console.log ("nombre2");
+//console.log ("nombre3");
+
+//seleccionar elementos
+
+let contenidoTitulo = "nombre"
+
+let titulo = document.querySelector(".logo.fuente-
+titulo.innerHTML = contenidoTitulo;
+//condicionales
+let textoTitulo = titulo.innerText;
+console.log(texto.Titulo); 
+if(textoTitulo)
